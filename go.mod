13fe96023aedd2dfe9a2b63f707c4d25420bf5b9
Szabolcs Toth <szasza.toth@gmail.com>
2022-11-26 00:36:15 +0000

    wip

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tothszabi/gogogo
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+)
+
+var (
+	version = ""
+	hash    = ""
+)
+
+func init() {
+	if version != "" && hash != "" {
+		return
+	}
+
+	info, available := debug.ReadBuildInfo()
+	if available && info.Main.Version != "" && info.Main.Sum != "" {
+		version = info.Main.Version
+		hash = fmt.Sprintf("mod sum: %q", info.Main.Sum)
+	} else {
+		version = "local"
+		hash = "unknown"
+	}
+}
+
+func main() {
+	log.Printf("Version: %s, hash: %s", version, hash)
+}
